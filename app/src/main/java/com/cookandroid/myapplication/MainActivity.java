cb892eca6fb7495481f3747b53a7a8b8e545b3cb
LinkinP7 <qkrwlstjd3@naver.com>
2020-11-07 19:25:57 +0900

    test

---
+++ app/src/main/java/com/cookandroid/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.cookandroid.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPrev, btnNext;
+        final ViewFlipper vFlipper;
+
+        btnPrev = (Button) findViewById(R.id.btnPrev);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        vFlipper = (ViewFlipper) findViewById(R.id.viewFlipper1);
+
+        btnPrev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vFlipper.showPrevious();
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vFlipper.showNext();
+            }
+        });
+    }
+}
+// B584020 박진성
